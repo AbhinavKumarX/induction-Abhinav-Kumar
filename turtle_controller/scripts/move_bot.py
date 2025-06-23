7c1fc5f5a0eef048e90588c538bdadff6e361f2c
AbhinavKumarX <abhi516nav@gmail.com>
2025-06-23 19:59:37 +0530

    first commit

---
+++ turtle_controller/scripts/move_bot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class MoveBot(Node):
+    def __init__(self):
+        super().__init__('move_bot')
+        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.get_logger().info("MoveBot node started, publishing Twist messages...")
+
+    def timer_callback(self):
+        msg = Twist()
+        msg.linear.x = 0.5
+        msg.angular.z = 1.0
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: linear={msg.linear.x}, angular={msg.angular.z}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MoveBot()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
